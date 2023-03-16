35e01bc198bd6cd383959bdb749cbc4f1a3a62d9
cunning <76720185+cunning-sys@users.noreply.github.com>
2023-03-16 10:46:18 -0700

    table

--- main.lua
+++ main.lua
@@ -1,1 +1,8 @@
+getgenv().catgirlcc = {
+    enabled = true,
+    predict_movement = true,
+    movement_prediction_type = 'catgirl.cc', -- // Roblox, catgirl.cc
+    movement_prediction = 0.119,
 
+    mode = 'Safe' -- // Safe, Blatant
+}
